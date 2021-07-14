3cd771bf64602cf760c278120d20f46490224bb6
S.Sinchana <70812303+Sinchana-Amin@users.noreply.github.com>
2021-07-14 09:16:45 +0530

    Update conn.java

--- conn.java
+++ conn.java
@@ -13,7 +13,7 @@ public class conn {
     public conn(){
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            c=DriverManager.getConnection("jdbc:mysql:///projecthms","root","JSK559@1973!#");
+            c=DriverManager.getConnection("jdbc:mysql:///projecthms","root","");
             s=c.createStatement();
         }
         catch(Exception e){
